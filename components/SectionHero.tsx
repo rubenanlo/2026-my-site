36ac10618b132b9096431dc6254af3b97bb0c6fb
rubenanlo <rubenanlo@gmail.com>
2025-08-21 16:27:08 +0200

    feat: added cosialicons, added functionanilty to track width of an element

--- components/SectionHero.tsx
+++ components/SectionHero.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { GitHubIcon, LinkedInIcon } from "@/components/SocialIcons";
 import Typewrite from "@/components/Typewrite";
 import { useAnimatedValue } from "@/lib/hooks/use-animated-value";
+import clsx from "clsx";
 import { motion } from "motion/react";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 type Card = {
   title: string;
@@ -29,7 +32,7 @@ const cards: Card[] = [
     value: [
       "React",
       "Next.js",
-      "Tailwind CSS",
+      "Tailwind",
       "TypeScript",
       "Javascript",
       "Python",
@@ -52,28 +55,52 @@ const cards: Card[] = [
 ];
 
 export default function SectionHero() {
+  const textRef = useRef<HTMLParagraphElement>(null);
+  const [textWidth, setTextWidth] = useState<number>(0);
+
+  const measureTextWidth = () => {
+    if (textRef.current) {
+      const width = textRef.current.getBoundingClientRect().width;
+      setTextWidth(width);
+      console.log("Text width:", width);
+    }
+  };
+
+  useEffect(() => {
+    measureTextWidth();
+
+    // Optional: Re-measure on window resize
+    const handleResize = () => measureTextWidth();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <section className="h-screen w-full flex items-center">
-      <div className="flex flex-col w-full h-3/4 sm:px-32 sm:py-20">
+      <div className="flex flex-col w-full h-3/4 sm:px-38 sm:py-20 justify-center">
         <div className="flex w-full">
-          <header className="flex flex-col px-4 flex-7">
+          <header className="flex flex-col flex-7 justify-between">
             <h1 className="text-4xl sm:text-6xl lg:text-7xl whitespace-nowrap">
               Ruben Andino
             </h1>
-            <h2 className="text-2xl sm:text-3xl lg:text-4xl">
+            <h2 className="text-2xl sm:text-3xl lg:text-4xl flex-1">
               Full Stack Web Developer
             </h2>
-            <p className="mt-20 sm:text-2xl">
+            <motion.p
+              ref={textRef}
+              className="mt-20 sm:text-2xl flex-2 w-fit max-w-3/4"
+            >
               Bridging the gap between business needs and web applications
-            </p>
+            </motion.p>
+            <SectionFooter style={{ width: textWidth }} />
           </header>
-          <div className="flex flex-col flex-1 w-full h-fit justify-start items-start gap-y-10">
+          <div className="flex flex-col flex-1 w-full h-fit justify-between items-between gap-y-10">
             {cards.map((card) => (
               <Card key={card.title} {...card} />
             ))}
           </div>
         </div>
-        <Links />
       </div>
     </section>
   );
@@ -112,7 +139,6 @@ const Card = ({
               <span className="text-4xl">+</span>
             </div>
           )}
-
           {animation === "typewriter" && (
             <Typewrite
               text={value as string[]}
@@ -133,10 +159,40 @@ const Card = ({
   );
 };
 
-const Links = () => {
+const SectionFooter = ({ style }: { style?: React.CSSProperties }) => {
   return (
-    <div className="flex items-center gap-x-2">
-      <Link href="/">h</Link>
+    <div className={"flex items-center gap-x-2"} style={style}>
+      <Link href="/">
+        <div className="p-[1px] bg-gradient rounded-md">
+          <LinkedInIcon className="w-[2.89rem] h-[2.89rem]" />
+        </div>
+      </Link>
+      <Link href="/">
+        <div className="p-[1px] bg-gradient rounded-md">
+          <GitHubIcon className="w-[2.89rem] h-[2.89rem]" />
+        </div>
+      </Link>
+      <Link href={`mailto:randinocv@gmail.com`} className="ml-auto">
+        <Button className="cursor-pointer">Contact me</Button>
+      </Link>
     </div>
+  );
+};
+
+const Button = ({
+  children,
+  className,
+  variant = "default",
+}: {
+  children: React.ReactNode;
+  className?: string;
+  variant?: "default";
+}) => {
+  const variants = {
+    default:
+      "px-10 py-3 bg-foreground-primary rounded-md text-primary font-bold",
+  };
+  return (
+    <button className={clsx(variants[variant], className)}>{children}</button>
   );
 };
